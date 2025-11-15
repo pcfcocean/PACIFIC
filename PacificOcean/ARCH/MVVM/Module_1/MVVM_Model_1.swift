7218e634422e816281a184645288296fb436b77e
pcfcocean <>
2025-11-15 19:24:28 +0300

    some code

--- PacificOcean/ARCH/MVVM/Module_1/MVVM_Model_1.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MVVM_Model.swift
-//  PacificOcaen
-//
-//  Created by Vlad Lesnichiy on 25.08.24.
-//
-
-import Foundation
--- PacificOcean/AsyncAwait/AsyncAwait_1.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AsyncAwait_1.swift
-//  PacificOcaen
-//
-//  Created by Vlad Lesnichiy on 22.06.25.
-//
-
-// Swift. Async/Await. Basics
-// https://www.youtube.com/watch?v=C2kBqexdvoI&list=PLtUCU7XGn_c48-Ixa0YiSEiGTFD4fxEAQ
